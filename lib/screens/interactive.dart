9529b563a80712fb0d44fff4b381478ad54dcb07
Prashu <1707prash@gmail.com>
2020-07-31 19:11:54 +0530

    Pages added

---
+++ lib/screens/interactive.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Interactive extends StatefulWidget {
+  @override
+  _InteractiveState createState() => _InteractiveState();
+}
+
+class _InteractiveState extends State<Interactive> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
