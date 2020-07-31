2a89ec6a1e54a0ea7e599d43ba6358fce4570447
Adrika Gupta <adrikagupta22@gmail.com>
2020-08-01 02:55:50 +0530

    Nav bar done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,11 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import '../models/user_model.dart';
+import 'addPost.dart';
+import 'examples.dart';
+import 'forum.dart';
+import 'infoPage.dart';
+import 'interactive.dart';
 
 class Home extends StatefulWidget {
   final User currentUser;
@@ -9,13 +15,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  PageController pageController;
+  int _selectedIndex=0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+  onPageChanged(int _selectedIndex){
+    setState(() {
+      this._selectedIndex=_selectedIndex;
+    });
+  }
+
+  onTap(int _selectedIndex){
+    pageController.animateToPage(_selectedIndex, duration: Duration(milliseconds: 40), curve:Curves.ease);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
-      body: Container(
-       
-      ),
+     
+      bottomNavigationBar: CurvedNavigationBar(
+        height: 60,
+        index:2,
+        color: Theme.of(context).primaryColor,
+        backgroundColor: Colors.white10,
+        items: <Widget>[
+          Icon(Icons.info, size: 30,color: Colors.white,),
+          Icon(Icons.home, size: 30,color: Colors.white,),
+          Icon(Icons.add, size: 37,color: Colors.white,),
+          Icon(Icons.file_upload, size: 30,color: Colors.white,),
+          Icon(Icons.forum, size: 30,color: Colors.white,),
+        ],
+        onTap: onTap,
+       ),
+       body: PageView(
+         children: <Widget>[
+           InfoPage(),
+           Example(),
+           AddPost(),
+           Interactive(),
+           Forum()
+         ],
+         controller:pageController,
+         physics: NeverScrollableScrollPhysics(),
+         onPageChanged: onPageChanged,
+       ),
     );
   }
 }
