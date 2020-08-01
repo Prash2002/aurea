39a71030d009b4e32f14328003c29d3105de1bed
Prashu <1707prash@gmail.com>
2020-08-01 20:55:29 +0530

    changes

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -11,5 +11,5 @@ List<ExCategory> categories = [
   ExCategory(name: 'Nature', image: 'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80', list: nature ),
   ExCategory(name: 'Architure', image: 'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80', list: arch ),
   ExCategory(name: 'Art', image: 'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80', list: art ),
-
+  ExCategory(name: 'Human', image: 'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80', list: human ),
 ];
