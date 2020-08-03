028a7bd7529f04938a10e52dbd43a20b71c1fee5
Prashu <1707prash@gmail.com>
2020-08-03 21:34:58 +0530

    Images added

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -6,14 +6,15 @@ class ExCategory{
   String name;
   List<Example> list;
   Color color;
-  ExCategory({this.name, this.image, this.list, this.color});
+  String bgimage;
+  ExCategory({this.name, this.image, this.list, this.color, this.bgimage});
 }
 
 List<ExCategory> categories = [
-  ExCategory(name: 'Nature', image: 'https://i.pinimg.com/originals/cf/b3/a2/cfb3a20c45001b3dc39ffc961c4374ed.png', list: nature , color: Colors.amber[200]),
-  ExCategory(name: 'Architecture', image: 'https://image.freepik.com/free-photo/amazing-taj-mahal_122035-991.jpg', list: arch ,color: Colors.blueGrey[300] ),
-  ExCategory(name: 'Art', image: 'https://blog.singulart.com/wp-content/uploads/2019/08/tour_img-312981-148.jpg', list: art ,color: Colors.redAccent[100]),
-  ExCategory(name: 'Human', image: 'https://golden-ratio.club/images/galleries/people/golden-ratio-nose-eyes.jpg', list: human,color: Colors.blue[200]),
-  ExCategory(name: 'Memes', image: 'https://golden-ratio.club/images/galleries/memes/fawcett-fibonacci-spiral-hair.jpeg', list: meme ,color: Colors.green[200]),
-  ExCategory(name: 'Techmology', image: 'https://golden-ratio.club/images/galleries/tech/golden-ratio-apple-logo.jpg', list: tech,color: Colors.lime[200] ),
+  ExCategory(name: 'Nature', image: 'https://i.pinimg.com/originals/cf/b3/a2/cfb3a20c45001b3dc39ffc961c4374ed.png', list: nature , color: Colors.amber[200], bgimage: 'https://image.freepik.com/free-photo/closeup-beautiful-attractive-nature-view-green-leaf-blurred-greenery-background-garden_105035-1025.jpg'),
+  ExCategory(name: 'Architecture', image: 'https://image.freepik.com/free-photo/amazing-taj-mahal_122035-991.jpg', list: arch ,color: Colors.blueGrey[300], bgimage: 'https://image.freepik.com/free-photo/red-brown-vintage-brick-wall-with-shabby-structure-horizontal-wide-brickwall-background-grungy-red-brick-blank-wall-texture-retro-house-facade-abstract-panoramic-web-banner-stonewall-surface_1253-1065.jpg'),
+  ExCategory(name: 'Art', image: 'https://blog.singulart.com/wp-content/uploads/2019/08/tour_img-312981-148.jpg', list: art ,color: Colors.redAccent[100], bgimage: 'https://image.freepik.com/free-vector/dreamy-sky-with-pine-trees-watercolor-background_10126-884.jpg'),
+  ExCategory(name: 'Human', image: 'https://golden-ratio.club/images/galleries/people/golden-ratio-nose-eyes.jpg', list: human,color: Colors.blue[200], bgimage: 'https://image.freepik.com/free-vector/dreamy-floral-background_53876-92903.jpg'),
+  ExCategory(name: 'Memes', image: 'https://golden-ratio.club/images/galleries/memes/fawcett-fibonacci-spiral-hair.jpeg', list: meme ,color: Colors.green[200], bgimage: 'https://www.livemememaker.com/assets/templates/12.jpg'),
+  ExCategory(name: 'Techmology', image: 'https://golden-ratio.club/images/galleries/tech/golden-ratio-apple-logo.jpg', list: tech,color: Colors.lime[200], bgimage: 'https://as1.ftcdn.net/jpg/02/20/76/28/500_F_220762865_x44kxk0IN1PJeJlAYXuXNcjh9hYOcdlQ.jpg'),
 ];
--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -32,14 +32,30 @@ class _CategoryState extends State<Category> {
               // https://ctl.s6img.com/society6/img/nS2tioSeTZNiorLujCno0Yb7Jbc/w_700/comforters/swatch/~artwork,fw_6000,fh_6000,iw_6000,ih_6000/s6-original-art-uploads/society6/uploads/misc/1b994293daef455e9b9afd8e0325b5e7/~~/small-white-polka-dots-with-yellow-background-comforters.jpg too bright
               //  https://wallpaperaccess.com/full/1548252.jpg not seen
               //  https://i.pinimg.com/736x/7e/bf/c0/7ebfc03902a1c7eca22e2e94b8ce93c2.jpg too plain
-              color: widget.color,
-              //  decoration: BoxDecoration(
-              //     image: DecorationImage(
-              //       image: NetworkImage('https://image.freepik.com/free-photo/high-angle-view-beautiful-sunflowers-yellow-surface_23-2147926997.jpg'),
-              //       // fit:BoxFit.fitWidth,
-              //       repeat: ImageRepeat.repeat
-              //     ),
-              //   ),
+
+              // TODAY
+              // https://image.freepik.com/free-photo/yellow-sunflowers-pink_91769-188.jpg eww af
+              //https://png.pngtree.com/thumb_back/fh260/background/20190223/ourmid/pngtree-pure-color-watercolor-graffiti-gradient-background-board-design-board-design-image_66713.jpg okayish
+              // https://previews.123rf.com/images/tampatra/tampatra1805/tampatra180500100/101683800-circuit-board-on-white-background-high-tech-technology-background-texture-pattern-abstract-illustrat.jpg ewwww
+
+
+              //FINAL
+              // https://image.freepik.com/free-photo/sunflower-blue-sky-background-sunny-day_223622-184.jpg eww
+              // https://image.freepik.com/free-photo/snow-covered-trees-against-sky_199223-1370.jpg not bad
+              // https://image.freepik.com/free-vector/red-watercolor-splash-background_1035-9220.jpg
+              // https://image.freepik.com/free-photo/closeup-beautiful-attractive-nature-view-green-leaf-blurred-greenery-background-garden_105035-1025.jpg
+              // https://image.freepik.com/free-photo/eastern-biege-arch-mosaic-carved-architecture-classic-columns-indian-style-decorative-architectural-frame_88088-432.jpg looks great but empty in center
+              // https://image.freepik.com/free-photo/decorated-wall-qutub-complex-delhi-india_163782-5296.jpg
+              // color: widget.color,
+              // https://image.freepik.com/free-photo/red-brown-vintage-brick-wall-with-shabby-structure-horizontal-wide-brickwall-background-grungy-red-brick-blank-wall-texture-retro-house-facade-abstract-panoramic-web-banner-stonewall-surface_1253-1065.jpg
+              height: height,
+               decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage('https://as1.ftcdn.net/jpg/02/20/76/28/500_F_220762865_x44kxk0IN1PJeJlAYXuXNcjh9hYOcdlQ.jpg'),
+                    fit:BoxFit.cover,
+                    repeat: ImageRepeat.repeat
+                  ),
+                ),
               child: ListView.builder(
                 scrollDirection: orientation== Orientation.portrait ?Axis.vertical :Axis.horizontal,
                 
